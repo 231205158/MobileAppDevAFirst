0c86629944a8bb7cc924ee8006a10f852ab879b6
Koki Hiromura <231205158@ccmailg.meijo-u.ac.jp>
2024-09-03 16:08:19 +0900

    2日目終了

--- app/src/main/java/jp/ac/meijou/android/s231205158/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s231205158/MainActivity.java
@@ -1,8 +1,6 @@
 package jp.ac.meijou.android.s231205158;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,33 +8,45 @@ import jp.ac.meijou.android.s231205158.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        binding.editTextText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                binding.text.setText(editable.toString());
-            }
-        });
+//        binding.editTextText.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//                binding.text.setText(editable.toString());
+//            }
+//        });
 
         setContentView(binding.getRoot());
         binding.mainIcon.setImageResource(R.drawable.next);
-        binding.firstButton.setOnClickListener(view -> {
-            binding.text.setText("reseted");
+
+        prefDataStore = PrefDataStore.getInstance(this);
+
+        binding.saveButton.setOnClickListener(view -> {
+            var text =binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
         });
     }
+
+    protected void onStart() {
+        super.onStart();
+        prefDataStore.getString("name").ifPresent(name -> binding.text.setText(name));
+
+    }
+
 }

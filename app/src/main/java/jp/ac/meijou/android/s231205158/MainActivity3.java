1d9069873eb7e6b7e33e6aeae72df00ebd1fd4e3
Koki Hiromura <231205158@ccmailg.meijo-u.ac.jp>
2024-09-05 02:25:55 +0900

    データの受け渡し

--- app/src/main/java/jp/ac/meijou/android/s231205158/MainActivity3.java
+++ app/src/main/java/jp/ac/meijou/android/s231205158/MainActivity3.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Optional;
 
 import jp.ac.meijou.android.s231205158.databinding.ActivityMain3Binding;
 
 public class MainActivity3 extends AppCompatActivity {
     private ActivityMain3Binding binding;
     private PrefDataStore prefDataStore;
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +23,16 @@ public class MainActivity3 extends AppCompatActivity {
         binding = ActivityMain3Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.buttonSend.setOnClickListener(view -> {
+             text = binding.nameForm.getText().toString();
+        });
+
         binding.specific.setOnClickListener(view -> {
-            var intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            var intent = new Intent(this, MainActivity4.class);
+            if (text != null) {
+                intent.putExtra("nameIntent", text);
+            }
+            getActivityResult.launch(intent);
         });
 
         binding.implicit.setOnClickListener(view -> {
@@ -30,4 +42,23 @@ public class MainActivity3 extends AppCompatActivity {
             startActivity(intent);
         });
     }
+
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()) {
+                    case RESULT_OK:
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getBooleanExtra("ret", false))
+                                .map(text -> "Result: " + text)
+                                .ifPresent(text -> binding.resultText.setText(text));
+                        break;
+                    case RESULT_CANCELED:
+                        binding.resultText.setText("Result: Canceled");
+                        break;
+                    default:
+                        binding.resultText.setText("Result: Unknown(" + result.getResultCode() + ")");
+                }
+            }
+    );
 }
